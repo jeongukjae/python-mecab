eb2c7d34a48fbf746624930dee5c540e73683058
jeongukjae <JeongUkJae@gmail.com>
2019-12-29 01:18:56 +0900

    remove unused

--- Includes/viterbi.h
+++ Includes/viterbi.h
@@ -1,8 +1,3 @@
-//  MeCab -- Yet Another Part-of-Speech and Morphological Analyzer
-//
-//
-//  Copyright(C) 2001-2006 Taku Kudo <taku@chasen.org>
-//  Copyright(C) 2004-2006 Nippon Telegraph and Telephone Corporation
 #ifndef MECAB_VITERBI_H_
 #define MECAB_VITERBI_H_
 
--- Sources/libmecab.cc
+++ Sources/libmecab.cc
@@ -1,8 +1,3 @@
-//
-//  MeCab -- Yet Another Part-of-Speech and Morphological Analyzer
-//
-//  Copyright(C) 2001-2006 Taku Kudo <taku@chasen.org>
-//  Copyright(C) 2004-2006 Nippon Telegraph and Telephone Corporation
 #if defined(_WIN32) && !defined(__CYGWIN__)
 #include <windows.h>
 #endif
