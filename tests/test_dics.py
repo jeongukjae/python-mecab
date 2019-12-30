4df52e4983509fa67cb2642d5844a9e0ae69d52b
jeongukjae <JeongUkJae@gmail.com>
2019-12-30 21:46:47 +0900

    assert is file and folder exists

--- tests/test_dics.py
+++ tests/test_dics.py
@@ -25,11 +25,17 @@ def test_dics(test_data_path, tmpdir):
     DIC_DIR = os.path.join("tests/test-data", test_data_path)
     PROCESSED_DIC_DIR = tmpdir.mkdir(test_data_path)
 
+    DICRC = os.path.join(DIC_DIR, "dicrc")
     TEST_CASE = os.path.join(DIC_DIR, "test")
     TRUE_PATH = os.path.join(DIC_DIR, "test.gld")
     PREDICT_PATH = PROCESSED_DIC_DIR.join("output.txt")
 
-    _copy_file(os.path.join(DIC_DIR, "dicrc"), PROCESSED_DIC_DIR.join("dicrc"))
+    assert os.path.exists(DIC_DIR)
+    assert os.path.exists(DICRC)
+    assert os.path.exists(TEST_CASE)
+    assert os.path.exists(TRUE_PATH)
+
+    _copy_file(DICRC, PROCESSED_DIC_DIR.join("dicrc"))
 
     run_mecab_dict_index(["index", "-f", "euc-jp", "-c", "euc-jp", "-d", DIC_DIR, "-o", str(PROCESSED_DIC_DIR)])
     run_mecab_main(["mecab", "-r", "/dev/null", "-d", str(PROCESSED_DIC_DIR), "-o", str(PREDICT_PATH), TEST_CASE])
