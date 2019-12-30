22317cab3eeb8b544ac570bc682c3a8255d8a64d
jeongukjae <JeongUkJae@gmail.com>
2019-12-30 21:34:37 +0900

    add run-dics test case

---
+++ tests/test_dics.py
@@ -0,0 +1,43 @@
+"""
+equivalent test of run-dics.sh in
+https://github.com/taku910/mecab/blob/master/mecab/tests/run-dics.sh
+"""
+import os
+
+import pytest
+
+from mecab.cli import run_mecab_dict_index, run_mecab_main
+
+
+@pytest.mark.parametrize(
+    "test_data_path",
+    [
+        pytest.param("autolink"),
+        pytest.param("chartype"),
+        pytest.param("katakana"),
+        pytest.param("latin"),
+        pytest.param("ngram"),
+        pytest.param("shiin"),
+        pytest.param("t9"),
+    ],
+)
+def test_dics(test_data_path, tmpdir):
+    DIC_DIR = os.path.join("tests/test-data", test_data_path)
+    PROCESSED_DIC_DIR = tmpdir.mkdir(test_data_path)
+
+    TEST_CASE = os.path.join(DIC_DIR, "test")
+    TRUE_PATH = os.path.join(DIC_DIR, "test.gld")
+    PREDICT_PATH = PROCESSED_DIC_DIR.join("output.txt")
+
+    _copy_file(os.path.join(DIC_DIR, "dicrc"), PROCESSED_DIC_DIR.join("dicrc"))
+
+    run_mecab_dict_index(["index", "-f", "euc-jp", "-c", "euc-jp", "-d", DIC_DIR, "-o", str(PROCESSED_DIC_DIR)])
+    run_mecab_main(["mecab", "-r", "/dev/null", "-d", str(PROCESSED_DIC_DIR), "-o", str(PREDICT_PATH), TEST_CASE])
+
+    with open(TRUE_PATH, "rb") as f:
+        assert f.read() == PREDICT_PATH.read(mode="rb")
+
+
+def _copy_file(from_, to):
+    with open(from_) as f:
+        to.write(f.read())
