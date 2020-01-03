d1408eb557aa49980cad4be79715db077f050fff
jeongukjae <JeongUkJae@gmail.com>
2020-01-03 23:49:14 +0900

    update include guard in tagger.h

--- mecab/_C/tagger.h
+++ mecab/_C/tagger.h
@@ -1,8 +1,8 @@
-#ifndef MECAB_PYTHON_TAGGER_H
-#define MECAB_PYTHON_TAGGER_H
+#ifndef _MECAB_PYTHON_TAGGER_H
+#define _MECAB_PYTHON_TAGGER_H
 
 #include "PythonCommon.h"
 
 bool initializeTaggerClass(PyObject* to);
 
-#endif  // MECAB_PYTHON_TAGGER_H
+#endif  // _MECAB_PYTHON_TAGGER_H
