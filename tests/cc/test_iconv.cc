dad514794b31f65134deb090ad60c144138f68cf
jeongukjae <JeongUkJae@gmail.com>
2020-01-12 14:34:19 +0900

    update test code for mecab/iconv.h

--- tests/cc/test_iconv.cc
+++ tests/cc/test_iconv.cc
@@ -1,14 +1,15 @@
+#include <fstream>
 #include "gtest/gtest.h"
 #include "mecab/iconv.h"
 
-TEST(mecab_iconv, test_convert_utf8_to_utf16_and_vice_versa) {
-  MeCab::Iconv iconv;
-  MeCab::Iconv iconv2;
-  iconv.open("UTF-8", "UTF-16");
-  iconv2.open("UTF-16", "UTF-8");
-  std::string str = "hello world";
-  iconv.convert(&str);
-  iconv2.convert(&str);
+TEST(mecab_iconv, test_convert_utf16_to_utf8) {
+  std::ifstream utf16File("../tests/test-data/cc/file-used-in-test-iconv-utf16.txt");
+  ASSERT_FALSE(utf16File.fail());
 
-  ASSERT_EQ(str, "hello world");
+  std::string utf16FileContent((std::istreambuf_iterator<char>(utf16File)), std::istreambuf_iterator<char>());
+
+  MeCab::Iconv iconv;
+  iconv.open("UTF-16", "UTF-8");
+  iconv.convert(&utf16FileContent);
+  ASSERT_EQ(utf16FileContent, "hello world\n");
 }
