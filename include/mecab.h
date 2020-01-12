4a166df24a6b2dd1fa6eb17b29b2a0abf95f4ffd
JeongUkJae <jeongukjae@gmail.com>
2020-01-12 17:15:09 +0900

    update struct definitions

--- include/mecab.h
+++ include/mecab.h
@@ -12,212 +12,6 @@
 /* C/C++ common data structures  */
 
 /**
- * DictionaryInfo structure
- */
-struct mecab_dictionary_info_t {
-  /**
-   * filename of dictionary
-   * On Windows, filename is stored in UTF-8 encoding
-   */
-  const char* filename;
-
-  /**
-   * character set of the dictionary. e.g., "SHIFT-JIS", "UTF-8"
-   */
-  const char* charset;
-
-  /**
-   * How many words are registered in this dictionary.
-   */
-  unsigned int size;
-
-  /**
-   * dictionary type
-   * this value should be MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
-   */
-  int type;
-
-  /**
-   * left attributes size
-   */
-  unsigned int lsize;
-
-  /**
-   * right attributes size
-   */
-  unsigned int rsize;
-
-  /**
-   * version of this dictionary
-   */
-  unsigned short version;
-
-  /**
-   * pointer to the next dictionary info.
-   */
-  struct mecab_dictionary_info_t* next;
-};
-
-/**
- * Path structure
- */
-struct mecab_path_t {
-  /**
-   * pointer to the right node
-   */
-  struct mecab_node_t* rnode;
-
-  /**
-   * pointer to the next right path
-   */
-  struct mecab_path_t* rnext;
-
-  /**
-   * pointer to the left node
-   */
-  struct mecab_node_t* lnode;
-
-  /**
-   * pointer to the next left path
-   */
-
-  struct mecab_path_t* lnext;
-
-  /**
-   * local cost
-   */
-  int cost;
-
-  /**
-   * marginal probability
-   */
-  float prob;
-};
-
-/**
- * Node structure
- */
-struct mecab_node_t {
-  /**
-   * pointer to the previous node.
-   */
-  struct mecab_node_t* prev;
-
-  /**
-   * pointer to the next node.
-   */
-  struct mecab_node_t* next;
-
-  /**
-   * pointer to the node which ends at the same position.
-   */
-  struct mecab_node_t* enext;
-
-  /**
-   * pointer to the node which starts at the same position.
-   */
-  struct mecab_node_t* bnext;
-
-  /**
-   * pointer to the right path.
-   * this value is NULL if MECAB_ONE_BEST mode.
-   */
-  struct mecab_path_t* rpath;
-
-  /**
-   * pointer to the right path.
-   * this value is NULL if MECAB_ONE_BEST mode.
-   */
-  struct mecab_path_t* lpath;
-
-  /**
-   * surface string.
-   * this value is not 0 terminated.
-   * You can get the length with length/rlength members.
-   */
-  const char* surface;
-
-  /**
-   * feature string
-   */
-  const char* feature;
-
-  /**
-   * unique node id
-   */
-  unsigned int id;
-
-  /**
-   * length of the surface form.
-   */
-  unsigned short length;
-
-  /**
-   * length of the surface form including white space before the morph.
-   */
-  unsigned short rlength;
-
-  /**
-   * right attribute id
-   */
-  unsigned short rcAttr;
-
-  /**
-   * left attribute id
-   */
-  unsigned short lcAttr;
-
-  /**
-   * unique part of speech id. This value is defined in "pos.def" file.
-   */
-  unsigned short posid;
-
-  /**
-   * character type
-   */
-  unsigned char char_type;
-
-  /**
-   * status of this model.
-   * This value is MECAB_NOR_NODE, MECAB_UNK_NODE, MECAB_BOS_NODE, MECAB_EOS_NODE, or MECAB_EON_NODE.
-   */
-  unsigned char stat;
-
-  /**
-   * set 1 if this node is best node.
-   */
-  unsigned char isbest;
-
-  /**
-   * forward accumulative log summation.
-   * This value is only available when MECAB_MARGINAL_PROB is passed.
-   */
-  float alpha;
-
-  /**
-   * backward accumulative log summation.
-   * This value is only available when MECAB_MARGINAL_PROB is passed.
-   */
-  float beta;
-
-  /**
-   * marginal probability.
-   * This value is only available when MECAB_MARGINAL_PROB is passed.
-   */
-  float prob;
-
-  /**
-   * word cost.
-   */
-  short wcost;
-
-  /**
-   * best accumulative cost from bos node to this node.
-   */
-  long cost;
-};
-
-/**
  * Parameters for MeCab::Node::stat
  */
 enum {
@@ -328,9 +122,212 @@ enum {
 
 /* C++ interface */
 namespace MeCab {
-typedef struct mecab_dictionary_info_t DictionaryInfo;
-typedef struct mecab_path_t Path;
-typedef struct mecab_node_t Node;
+
+/**
+ * DictionaryInfo structure
+ */
+struct DictionaryInfo {
+  /**
+   * filename of dictionary
+   * On Windows, filename is stored in UTF-8 encoding
+   */
+  const char* filename;
+
+  /**
+   * character set of the dictionary. e.g., "SHIFT-JIS", "UTF-8"
+   */
+  const char* charset;
+
+  /**
+   * How many words are registered in this dictionary.
+   */
+  unsigned int size;
+
+  /**
+   * dictionary type
+   * this value should be MECAB_USR_DIC, MECAB_SYS_DIC, or MECAB_UNK_DIC.
+   */
+  int type;
+
+  /**
+   * left attributes size
+   */
+  unsigned int lsize;
+
+  /**
+   * right attributes size
+   */
+  unsigned int rsize;
+
+  /**
+   * version of this dictionary
+   */
+  unsigned short version;
+
+  /**
+   * pointer to the next dictionary info.
+   */
+  struct DictionaryInfo* next;
+};
+
+/**
+ * Path structure
+ */
+struct Path {
+  /**
+   * pointer to the right node
+   */
+  struct Node* rnode;
+
+  /**
+   * pointer to the next right path
+   */
+  struct Path* rnext;
+
+  /**
+   * pointer to the left node
+   */
+  struct Node* lnode;
+
+  /**
+   * pointer to the next left path
+   */
+
+  struct Path* lnext;
+
+  /**
+   * local cost
+   */
+  int cost;
+
+  /**
+   * marginal probability
+   */
+  float prob;
+};
+
+/**
+ * Node structure
+ */
+struct Node {
+  /**
+   * pointer to the previous node.
+   */
+  struct Node* prev;
+
+  /**
+   * pointer to the next node.
+   */
+  struct Node* next;
+
+  /**
+   * pointer to the node which ends at the same position.
+   */
+  struct Node* enext;
+
+  /**
+   * pointer to the node which starts at the same position.
+   */
+  struct Node* bnext;
+
+  /**
+   * pointer to the right path.
+   * this value is NULL if MECAB_ONE_BEST mode.
+   */
+  struct Path* rpath;
+
+  /**
+   * pointer to the right path.
+   * this value is NULL if MECAB_ONE_BEST mode.
+   */
+  struct Path* lpath;
+
+  /**
+   * surface string.
+   * this value is not 0 terminated.
+   * You can get the length with length/rlength members.
+   */
+  const char* surface;
+
+  /**
+   * feature string
+   */
+  const char* feature;
+
+  /**
+   * unique node id
+   */
+  unsigned int id;
+
+  /**
+   * length of the surface form.
+   */
+  unsigned short length;
+
+  /**
+   * length of the surface form including white space before the morph.
+   */
+  unsigned short rlength;
+
+  /**
+   * right attribute id
+   */
+  unsigned short rcAttr;
+
+  /**
+   * left attribute id
+   */
+  unsigned short lcAttr;
+
+  /**
+   * unique part of speech id. This value is defined in "pos.def" file.
+   */
+  unsigned short posid;
+
+  /**
+   * character type
+   */
+  unsigned char char_type;
+
+  /**
+   * status of this model.
+   * This value is MECAB_NOR_NODE, MECAB_UNK_NODE, MECAB_BOS_NODE, MECAB_EOS_NODE, or MECAB_EON_NODE.
+   */
+  unsigned char stat;
+
+  /**
+   * set 1 if this node is best node.
+   */
+  unsigned char isbest;
+
+  /**
+   * forward accumulative log summation.
+   * This value is only available when MECAB_MARGINAL_PROB is passed.
+   */
+  float alpha;
+
+  /**
+   * backward accumulative log summation.
+   * This value is only available when MECAB_MARGINAL_PROB is passed.
+   */
+  float beta;
+
+  /**
+   * marginal probability.
+   * This value is only available when MECAB_MARGINAL_PROB is passed.
+   */
+  float prob;
+
+  /**
+   * word cost.
+   */
+  short wcost;
+
+  /**
+   * best accumulative cost from bos node to this node.
+   */
+  long cost;
+};
 
 template <typename N, typename P>
 class Allocator;
@@ -1032,13 +1029,6 @@ extern void deleteTagger(Tagger* tagger);
  * @return error string
  */
 extern const char* getLastError();
-
-/**
- * An alias of getLastError.
- * It is kept for backward compatibility.
- * @return error string
- */
-extern const char* getTaggerError();
 }  // namespace MeCab
 
 int mecab_do(int argc, char** argv);
