467ea871605586c9fc3234ef0c7ea0bad12fe7c0
JeongUkJae <jeongukjae@gmail.com>
2020-02-07 00:17:24 +0900

    merge Cmake codes

--- cmake/CodeCoverage.cmake
+++
@@ -1,6 +0,0 @@
-option(BUILD_COV "Build for code coverage" OFF)
-
-if(BUILD_COV AND CMAKE_BUILD_TYPE MATCHES Debug)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 --coverage")
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --coverage")
-endif()
